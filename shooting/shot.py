7dcd2423b05d4ca72453fbbe8e5a23ae173defb3
chad cavender <chadc265@gmail.com>
2021-04-26 07:04:59 -0400

    added is shot good in 2D to shot.py

--- shooting/shot.py
+++ shooting/shot.py
@@ -1,7 +1,8 @@
 from math import pi, atan2, asin, sin, cos
 from rlbot.agents.base_agent import SimpleControllerState
 from rlbot.utils.rendering.rendering_manager import RenderingManager
-from rlutilities.linear_algebra import vec3, normalize, norm
+from rlutilities.linear_algebra import vec3, normalize, norm, cross, vec2
+from rlutilities.simulation import Car
 from util.math_funcs import sign, clamp_in_field, clamp, clamp_vecs
 
 class Shot:
@@ -26,3 +27,16 @@ class Shot:
     @property
     def mid_shot(self):
         return self.ball_position + self.vec_to_mid * -1.2 * self.BALL_RADIUS
+
+    def is_approach_good(self, car: Car):
+        car_vel = vec2(car.velocity.x, car.velocity.y)
+        right = vec2(self.vec_to_right.x, self.vec_to_right.y)
+        left = vec2(self.vec_to_left.x, self.vec_to_left.y)
+        right_x_left = cross(right, left)
+        right_x_vel = cross(right, car_vel)
+        if right_x_left * right_x_vel >= 0:
+            vel_x_left = cross(car_vel, left)
+            vel_x_right = cross(car_vel, right)
+            if vel_x_left * vel_x_right >= 0:
+                return True
+        return False
